3768ace0ed2bbefb0cc9c162099fda8d4bbec8e2
shivam <shivamsahu13@gmail.com>
2026-08-10 22:08:38 +0530

    code updated

--- src/com/shivam/day05/Maximum.java
+++ src/com/shivam/day05/Maximum.java
@@ -6,12 +6,16 @@ public class Maximum {
     }
 
     public static int findMaximum(int a, int b, int c){
-        if(a > b && a > c){
-            return a;
-        }else if(b > a && b > c){
-            return b;
-        }else{
-            return c;
+        int max = a;
+
+        if(b > max){
+            max = b;
         }
+
+        if(c > max){
+            max = c;
+        }
+
+        return max;
     }
 }
